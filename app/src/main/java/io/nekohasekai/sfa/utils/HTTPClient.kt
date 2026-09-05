a60933325bf383a1943afae9e220a21c6a143ab3
nekolsd <nekolsd@proton.me>
2026-09-05 23:41:27 +0800

    http: trust built-in Mozilla and Chrome certificate stores

--- app/src/main/java/io/nekohasekai/sfa/utils/HTTPClient.kt
+++ app/src/main/java/io/nekohasekai/sfa/utils/HTTPClient.kt
@@ -21,6 +21,8 @@ class HTTPClient : Closeable {
 
     init {
         client.modernTLS()
+        client.appendCertificateStore("mozilla")
+        client.appendCertificateStore("chrome")
     }
 
     fun getString(url: String): String {
